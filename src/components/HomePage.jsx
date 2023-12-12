70dfca3f8c45dcdf12f86b1660c8a74c126399ae
parkheeyu <qkrgmldbs123@gmail.com>
2023-12-12 17:49:43 +0900

    20231212

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+import port from './portpolio.png';
+
 const HomePage = () => {
     return (
 
-        <Row className='my-5'>
+        <Row className='my-5 text-center'>
             <Col>
-                <h1 className='text-center'>홈111111</h1>
+                <img src={port} alt="포트폴리오"
+                    width="90%"
+                    
+                     />
             </Col>
         </Row>
 
